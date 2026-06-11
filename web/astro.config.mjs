11637cbfe18a444ca055dd037c6040ef8470ed2d
Diego Sasco <elegomf@gmail.com>
2026-06-10 22:22:50 -0300

    fix: update basePath to /cv/ after repo rename

--- web/astro.config.mjs
+++ web/astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://sascodiego.github.io",
-	base: "/cv-pipeline/",
+	base: "/cv/",
 });
